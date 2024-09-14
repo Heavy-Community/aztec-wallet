68788109b52303f61385b94aca92763be263a2ec
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-09-14 17:24:54 +0300

    feat: Implement the Babyjubjub account contract and library verification for baby jubjub

---
+++ src/accounts/baby jubjub/account_contract.ts
@@ -0,0 +1,33 @@
+import { DefaultAccountContract } from "@aztec/accounts/defaults";
+import { BabyJubJub, BabyJubJubScalar } from ".";
+import { AuthWitness, AuthWitnessProvider, CompleteAddress, ContractArtifact } from "@aztec/aztec.js";
+import { type Fr } from '@aztec/foundation/fields';
+/**
+ * Account contract that authenticates transactions using BabyJubJub signatures
+ * verified against a BabyJubJub public key stored in an immutable encrypted note.
+ */
+export class BabyJubJubAccountContract extends DefaultAccountContract {
+    constructor(private signingPrivateKey: BabyJubJubScalar) {
+        super(BabyJubJubAccountContractArtifact as ContractArtifact);
+    }
+
+    getDeploymentArgs() {
+        const signingPublicKey = new BabyJubJub().computePublicKey(this.signingPrivateKey);
+        return [signingPublicKey.x, signingPublicKey.y];
+    }
+
+    getAuthWitnessProvider(_address: CompleteAddress): AuthWitnessProvider {
+        return new BabyJubJubAuthWitnessProvider(this.signingPrivateKey);
+    }
+}
+
+/** Creates auth witnesses using BabyJubJub signatures. */
+class BabyJubJubAuthWitnessProvider implements AuthWitnessProvider {
+    constructor(private signingPrivateKey: BabyJubJubScalar) { }
+
+    createAuthWit(messageHash: Fr): Promise<AuthWitness> {
+        const babyJubJub = new BabyJubJub();
+        const signature = babyJubJub.constructSignature(messageHash.toBuffer(), this.signingPrivateKey).toBuffer();
+        return Promise.resolve(new AuthWitness(messageHash, [...signature]));
+    }
+}
