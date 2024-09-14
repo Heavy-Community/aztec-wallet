102320054f527192ed726b585ea10a68c4392cc2
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-09-15 02:08:53 +0300

    Attempt to deploy the ecdsa account contract trough grumpkin point

--- src/hooks/useEcdsa.tsx
+++ src/hooks/useEcdsa.tsx
@@ -8,7 +8,9 @@ import {
   ContractArtifact,
   ContractInstanceWithAddress,
   createPXEClient,
+  Fq,
   Fr,
+  Grumpkin,
   GrumpkinScalar,
   NodeInfo,
   waitForPXE,
@@ -23,59 +25,23 @@ import { AccountInterface } from "@aztec/aztec.js/account";
 import { DefaultAccountInterface } from "@aztec/accounts/defaults";
 import { setupSandbox } from "./utils";
 
-const PRIVATE_KEY = GrumpkinScalar.fromString(
-  "0xd35d743ac0dfe3d6dbe6be8c877cb524a00ab1e3d52d7bada095dfc8894ccfa"
-).toBuffer();
-const SIGNING_PUBLIC_KEY = new Ecdsa().computePublicKey(PRIVATE_KEY);
+const ecc_grumpkin = new Grumpkin();
+const secretKey = GrumpkinScalar.random();
+const PUBLIC_KEY = ecc_grumpkin.mul(Grumpkin.generator, secretKey);
+const PUBLIC_KEY_X = bigIntToNumberArray(PUBLIC_KEY.x.toBigInt());
+const PUBLIC_KEY_Y = bigIntToNumberArray(PUBLIC_KEY.y.toBigInt());
 
-class ECDSAAccountContract
-  extends EcdsaKAccountContract
-  implements AccountContract
-{
-  constructor(
-    private privateKey = PRIVATE_KEY,
-    wallet: Wallet,
-    instance: ContractInstanceWithAddress
-  ) {
-    super(instance, wallet);
+function bigIntToNumberArray(value: bigint): number[] {
+  const hex = value.toString(16).padStart(64, "0");
+  const result: number[] = [];
+  for (let i = 0; i < 32; i++) {
+    result.push(parseInt(hex.substr(i * 2, 2), 16));
   }
-
-  getContractArtifact(): ContractArtifact {
-    return this.artifact;
-  }
-
-  getInterface(address: CompleteAddress, nodeInfo: NodeInfo): AccountInterface {
-    return new DefaultAccountInterface(
-      this.getAuthWitnessProvider(address),
-      address,
-      nodeInfo
-    );
-  }
-
-  getDeploymentArgs(): any[] | undefined {
-    return undefined;
-  }
-
-  getAuthWitnessProvider(address: CompleteAddress): AuthWitnessProvider {
-    const privateKey = this.privateKey;
-    return {
-      createAuthWit(messageHash: Fr): Promise<AuthWitness> {
-        const signer = new Ecdsa();
-        const signature = signer.constructSignature(
-          messageHash.toBuffer(),
-          privateKey
-        );
-        return Promise.resolve(
-          new AuthWitness(messageHash, [...signature.toBuffer()])
-        );
-      },
-    };
-  }
+  return result;
 }
 
 export function useEcdsa() {
   const [wait_ecdsa, setWait] = useState(false);
-  const [contract, setContract] = useState<Contract | undefined>();
   const [new_account_addres, setNewAccountAddress] = useState<string | null>(
     null
   );
@@ -90,54 +56,31 @@ export function useEcdsa() {
       const salt = Fr.random();
       const wallet = await deployerEnv.getWallet();
 
-      const deployment_tx = ECDSAAccountContract.deploy(
-        wallet,
-        SIGNING_PUBLIC_KEY.subarray(0, 32) as any,
-        SIGNING_PUBLIC_KEY.subarray(32, 64) as any
-      ).send({
-        contractAddressSalt: salt,
-      });
-
-      // private key or imported instance.deploy() | instance.sign()
-      // await pxe.registerAccount(, deployment_tx.instance.address);
+      const contract = await toast.promise(
+        EcdsaKAccountContract.deploy(wallet, PUBLIC_KEY_X, PUBLIC_KEY_Y)
+          .send()
+          .wait(),
+        {
+          pending: "Deploying contract...",
+          success: {
+            render: ({ data }) => `Address: ${data.contract.address}`,
+          },
+          error: "Error deploying contract",
+        }
+      );
+      await pxe.registerContract(contract.contract);
+      let secretKeyFq = new Fr(new Fq(secretKey).toBigInt());
+      await pxe.registerAccount(secretKeyFq, contract.contract.partialAddress);
+      setNewAccountAddress(contract.contract.address.toString());
+      setAddresses((prevAddresses) => [
+        ...prevAddresses,
+        contract.contract.address.toString(),
+      ]);
 
       console.log(
-        "deployment_tx.instance.address: ",
-        deployment_tx.instance.address
+        "New account's address: ",
+        contract.contract.address.toString()
       );
-      console.log(
-        "deployment_tx.instance.publicKeysHash: ",
-        deployment_tx.instance.publicKeysHash
-      );
-
-      // Awaits the buffer that contains the hash
-      // let buffer_hash = (await deployment_tx.getTxHash()).buffer;
-      // console.log("buffer_hash", buffer_hash);
-
-      // await wallet.createAuthWit(buffer_hash);
-
-      const contract = await toast.promise(deployment_tx.deployed(), {
-        pending: "Deploying contract...",
-        success: {
-          render: ({ data }) => `Address: ${data.address}`,
-        },
-        error: "Error deploying contract",
-      });
-
-      const accountAddressStr = (
-        await deployment_tx.deployed()
-      ).address.toString();
-
-      // let k = await pxe.registerAccount(salt, contract.partialAddress);
-      // console.log("k is: ", k);
-      // console.log("contract.partialAddress is: ", contract.partialAddress);
-
-      setNewAccountAddress(accountAddressStr);
-      setAddresses((prevAddresses) => [...prevAddresses, accountAddressStr]); // Add new address to the array
-      setContract(contract);
-
-      console.log("New account's address: ", accountAddressStr);
-      console.log("addresses are: ", ecdsa_addresses);
     } catch (error) {
       console.error("Error creating account: ", error);
     } finally {
@@ -147,7 +90,6 @@ export function useEcdsa() {
 
   return {
     createEcdsaAccount,
-    contract,
     wait_ecdsa,
     new_account_addres,
     ecdsa_addresses,
